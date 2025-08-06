c4c279f979f51c019b3f9a15dd546793242cca29
Purvi K <p57474414@gmail.com>
2025-08-06 13:28:50 +0530

    Add files via upload

---
+++ Day-2/printintfloatchar.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main (){
+
+
+    int age = 25;
+    float height  = 5.9;
+    char grade = 'A';
+
+    printf("Age : %d\n", age);
+    printf("Height : %.2lf\n", height);
+    printf("Grade : %c\n", grade);
+
+
+    return 0;
+}
