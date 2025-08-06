54decd165c69a6b9b671b42bbd28e70ec1b211f0
Purvi K <p57474414@gmail.com>
2025-08-06 13:31:07 +0530

    Add files via upload

---
+++ Day-3/simplecalculator.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+
+
+    int a ,b;
+    printf("Enter two numbers:");
+    scanf("%d %d", &a, &b);
+
+    printf("Sum :%d\n", a + b);
+    printf("Difference : %d\n", a - b);
+    printf("product : %d\n", a * b);
+    printf("quotient: %d\n", a / b);
+    
+
+
+    return 0;
+}
