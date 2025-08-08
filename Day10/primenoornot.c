e22586cd07243a8fc9951346cc1ec900ce07eafb
Purvi K <p57474414@gmail.com>
2025-08-08 16:06:49 +0530

    Add files via upload

---
+++ Day10/primenoornot.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool isPrime(int num){
+
+    if(num <= 1){
+
+            return false;
+    }
+    for(int i = 2 ; i <= num /2 ;i++){
+            if( num % i ==  0){
+   return false;
+            }
+}
+ return true;
+
+}
+
+int main(){
+
+    int num;
+
+    printf("enter a number:");
+    scanf("%d",&num);
+
+    if(isPrime(num)){
+printf("%d is a prime\n",num);
+    }
+
+    else {
+
+printf("%d is not a prime\n",num);
+
+    }
+
+    return 0;
+}
