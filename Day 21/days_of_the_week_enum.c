90232a8e4adc37f53caaa6d7b0498a91e2bc43da
Purvi K <p57474414@gmail.com>
2025-08-22 09:44:02 +0530

    Add files via upload

---
+++ Day 21/days_of_the_week_enum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+enum Day{ SUNDAY , MONDAY , TUESDAY , WEDNESDAY , THURSDAY , FRIDAY , SATURDAY};
+
+int main(){
+
+
+enum Day today =  WEDNESDAY;
+if(today == WEDNESDAY){
+    printf("Midweek!\n");
+}
+
+
+
+
+    return 0;
+}
